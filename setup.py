2b0b31b5fc08100ba02045dae21179be9f8d5f36
jmtyson <jeff.tyson@gmail.com>
2014-12-26 23:50:15 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pyrasplcd',
+      version='0.3',
+      description='Have your embedded machine print how it feels.',
+      url='https://github.com/jmtyson/pyrasplcd',
+      author='Jeffrey M Tyson',
+      author_email='jeff.tyson@gmail.com',
+      license='MIT',
+      packages=['pyrasplcd'],
+      install_requires=['pyserial','PIL'],
+      zip_safe=False)
